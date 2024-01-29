abe968b7403b305972822031c693a3f130b05047
Azul <azulackerl912@gmail.com>
2024-01-29 09:30:01 -0300

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Messi']);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    // console.log(newCategory);
+    // categories.push(newCategory);
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory onNewCategory={(event) => onAddCategory(event)} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
